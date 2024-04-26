78dc0b635865a6854d1828003aec63b64e2d808a
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-04-27 00:13:55 +0545

    🔧 chore: Update package.json and pnpm-lock.yaml to add @types/cors dependency

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,16 @@ import createHttpError from "http-errors";
 import { globalErrorHandler } from "./globalErrorHandler";
 import userRouter from "./users/userRouter";
 import bookRouter from "./book/bookRouter";
+import cors from "cors";
+import { config } from "./config/config";
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: config.frontendUrl,
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
